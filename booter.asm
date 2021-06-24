80e0074620e4e5ad078144ef51b8744aa03f50a4
taozi <yitaom2@illinois.edu>
2021-06-24 11:51:06 -0500

    07.24.2021

--- booter.asm
+++ booter.asm
@@ -1,24 +1,31 @@
-mov ah, 0x0e
+[org 0x7c00]
+  mov [BOOT_DRIVE], dl
 
-mov bp, 0x8000
-mov sp, bp
+  mov bp, 0x8000
+  mov sp, bp
 
-push 'A'
-push 'B'
-push 'C'
+  mov bx, 0x9000
+  mov dh, 2
+  mov dl, [BOOT_DRIVE]
+  call disk_load
 
-pop bx
-mov al, bl
-int 0x10
+  mov dx, [0x9000]
+  call print_hex
 
-pop bx
-mov al, bl
-int 0x10
+  mov dx, [0x9000 + 512]
+  call print_hex
 
-mov al, [0x8000]
-int 0x10
+  jmp $
 
-jmp $
+%include "boot_sect_print.asm"
+%include "boot_sect_print_hex.asm"
+%include "disk_load.asm"
 
-times 510-($-$$) db 0
+BOOT_DRIVE: db 0
+
+times 510 - ($-$$) db 0
 dw 0xaa55
+
+
+times 256 dw 0xdada
+times 256 dw 0xface
