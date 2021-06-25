5b625a14d7f5c37e46aced1b808ba3d1d5675dd2
taozi <yitaom2@illinois.edu>
2021-06-25 14:58:55 -0500

    06.25.2021

--- booter.asm
+++ booter.asm
@@ -1,31 +1,27 @@
-[org 0x7c00]
-  mov [BOOT_DRIVE], dl
+[org 0x7c00] ; bootloader offset
+    mov bp, 0x9000 ; set the stack
+    mov sp, bp
 
-  mov bp, 0x8000
-  mov sp, bp
+    mov bx, MSG_REAL_MODE
+    call print ; This will be written after the BIOS messages
 
-  mov bx, 0x9000
-  mov dh, 2
-  mov dl, [BOOT_DRIVE]
-  call disk_load
-
-  mov dx, [0x9000]
-  call print_hex
-
-  mov dx, [0x9000 + 512]
-  call print_hex
-
-  jmp $
+    call switch_to_pm
+    jmp $ ; this will actually never be executed
 
 %include "boot_sect_print.asm"
-%include "boot_sect_print_hex.asm"
-%include "disk_load.asm"
+%include "gdt.asm"
+%include "print_string_pm.asm"
+%include "32bit-switch.asm"
 
-BOOT_DRIVE: db 0
+[bits 32]
+BEGIN_PM: ; after the switch we will get here
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm ; Note that this will be written at the top left corner
+    jmp $
 
-times 510 - ($-$$) db 0
+MSG_REAL_MODE db "Started in 16-bit real mode", 0
+MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
+
+; bootsector
+times 510-($-$$) db 0
 dw 0xaa55
-
-
-times 256 dw 0xdada
-times 256 dw 0xface
