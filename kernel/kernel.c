8f0013ec8289377e3fba423198ecc31470979ae2
taozi <yitaom2@illinois.edu>
2021-06-29 10:46:22 -0500

    07.29.2021

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,16 +1,9 @@
 #include "../drivers/ports.h"
+#include "../cpu/isr.h"
 
 void main() {
-  clear_screen();
-
-  /* Fill up the screen */
-  int i = 0;
-  for (i = 0; i < 24; i++) {
-      char str[255];
-      int_to_ascii(i, str);
-      kprint_at(str, 0, i);
-  }
-
-  kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
-  kprint("And with this text, the kernel will scroll again, and row 1 will disappear too!");
+  isr_install();
+  /* Test the interrupts */
+  __asm__ __volatile__("int $2");
+  __asm__ __volatile__("int $3");
 }
