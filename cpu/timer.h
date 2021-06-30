662f929f02f5c72c128dbcda952e7138ac510f4c
taozi <yitaom2@illinois.edu>
2021-06-30 11:22:54 -0500

    06.30.2021

--- cpu/timer.h
+++ cpu/timer.h
@@ -1,7 +1,7 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-#include "types.h"
+#include "type.h"
 #include "../libc/mem.h"
 #include "../libc/string.h"
 
--- drivers/screen.c
+++ drivers/screen.c
@@ -106,7 +106,8 @@ void kprint_backspace() {
     int offset = get_cursor_offset()-2;
     int row = get_offset_row(offset);
     int col = get_offset_col(offset);
-    print_char(0x08, col, row, WHITE_ON_BLACK);
+    print_char(' ', col, row, WHITE_ON_BLACK);
+    set_cursor_offset(offset);
 }
 
 void kprint(char *message) {
